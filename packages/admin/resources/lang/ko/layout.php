c265d6cccccf764e083bd6560e7fa765b90687cc
silnex <silnex@silnex.kr>
2022-11-04 12:21:43 +0900

    Add FA translations in korean langs file

--- packages/admin/resources/lang/ko/layout.php
+++ packages/admin/resources/lang/ko/layout.php
@@ -10,12 +10,28 @@ return [
             'label' => '다크모드',
         ],
 
+        'database_notifications' => [
+            'label' => '알림창 열기',
+        ],
+
         'light_mode' => [
             'label' => '라이트모드',
         ],
 
         'logout' => [
             'label' => '로그아웃',
+        ],
+
+        'sidebar' => [
+
+            'collapse' => [
+                'label' => '사이드바 닫기',
+            ],
+
+            'expand' => [
+                'label' => '사이드바 펴기',
+            ],
+
         ],
 
         'user_menu' => [
